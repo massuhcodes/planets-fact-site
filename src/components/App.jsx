9c483f54290d0dfadb475489bcc26092e7a9e9de
massuhcodes <massuhcodes@gmail.com>
2022-10-13 02:11:17 -0400

    Netlify testing 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
+import { planetData } from "/src/planet-data";
 import "/src/styles/App.css";
 import Header from "/src/components/Header";
 import Main from "/src/components/Main";
 
 function App() {
+    function compressOverhead() {
+        document.getElementById("hamburger").style.opacity = "1";
+        document.getElementById("overhead").classList.remove("expanded");
+    }
+
     return (
         <div className="app">
-            <Header />
+            <Header compressOverhead={compressOverhead} />
             <Main />
         </div>
     );
