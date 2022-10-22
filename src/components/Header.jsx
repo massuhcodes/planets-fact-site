c1b451caf71cff06c5e0834776f2d1598ece9c97
massuhcodes <massuhcodes@gmail.com>
2022-10-22 02:44:14 -0400

    Adjusted code for responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,74 +1,76 @@
 import "/src/styles/Header.css";
+import { nanoid } from "nanoid";
+import { expandOverhead, features } from "../utils/utils";
 import hamburger from "/src/assets/icon-hamburger.svg";
 import chevron from "/src/assets/icon-chevron.svg";
 
 export default function Header(props) {
-    const choices = props.names.map((planet, index) => {
-        let color;
-        switch (planet) {
-            case "mercury":
-                color = "#DEF4FC";
-                break;
-            case "venus":
-                color = "#F7CC7F";
-                break;
-            case "earth":
-                color = "#545BFE";
-                break;
-            case "mars":
-                color = "#FF6A45";
-                break;
-            case "jupiter":
-                color = "#ECAD7A";
-                break;
-            case "saturn":
-                color = "#FCCB6B";
-                break;
-            case "uranus":
-                color = "#65F0D5";
-                break;
-            case "neptune":
-                color = "#497EFA";
-                break;
-            default:
-                break;
-        }
+    // planet choices for mobile layout
+    const verticalPlanetChoices = props.planets.map((planet, index) => {
         return (
             <div
-                key={index.toString()}
-                className={`planet-choice common ${
-                    planet !== "neptune" ? "with-bottom-border" : ""
-                }`}
+                key={nanoid()}
+                className="vertical-planet-choice"
                 onClick={() => props.getChoice(index)}
             >
-                <div className="dot--name">
-                    <span className="dot" style={{ backgroundColor: color }} />
-                    <p>{planet.toUpperCase()}</p>
+                <div className="dot-name-container">
+                    <span
+                        className="dot"
+                        style={{ backgroundColor: planet.color }}
+                    />
+                    <span className="name">{planet.name.toUpperCase()}</span>
                 </div>
                 <img src={chevron} />
             </div>
         );
     });
 
-    function expandOverhead() {
-        document.getElementById("hamburger").style.opacity = "0.1";
-        document.getElementById("overhead").classList.add("expanded");
-    }
+    // planet choices for tablet, desktop, etc. layouts
+    const horizontalPlanetChoices = props.planets.map((planet, index) => (
+        <li
+            key={nanoid()}
+            className="horizontal-planet-choice"
+            onClick={() => props.getChoice(index)}
+            style={
+                planet === props.chosenPlanet
+                    ? { color: props.color }
+                    : { color: "#ffffff80" }
+            }
+        >
+            {planet.name.toUpperCase()}
+        </li>
+    ));
+
+    // Header component's features (mobile)
+    const featureElements = features.map((feature) => (
+        <li
+            key={nanoid()}
+            id={feature}
+            onClick={() => props.switchFeatureTo(feature)}
+            style={
+                props.feature === feature
+                    ? { color: "#fff", borderBottomColor: props.color }
+                    : {}
+            }
+        >
+            {feature.toUpperCase()}
+        </li>
+    ));
 
     return (
         <div className="Header-container">
-            <header className="common">
-                <h2>THE PLANETS</h2>
+            <header>
+                <h2>{"the planets".toUpperCase()}</h2>
                 <img id="hamburger" src={hamburger} onClick={expandOverhead} />
+                {/** tablet, desktop, etc. */}
+                <ul className="horizontal-planet-choice-container">
+                    {horizontalPlanetChoices}
+                </ul>
             </header>
-            <div className="overhead-container common" id="overhead">
-                {choices}
+            <div className="overhead-container" id="overhead">
+                {verticalPlanetChoices}
             </div>
-            <ul className="Header-subheader common">
-                <li>OVERVIEW</li>
-                <li>STRUCTURE</li>
-                <li>SURFACE</li>
-            </ul>
+            <ul className="features">{featureElements}</ul>
         </div>
     );
 }
