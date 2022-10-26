7d04f8ed3e398d5dc09a95179591c3a253d4f09a
massuhcodes <massuhcodes@gmail.com>
2022-10-26 15:40:51 -0400

    Modified Facts jsx file by refining code structure

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -1,19 +1,25 @@
+// Facts.jsx
+
 import "/src/styles/Facts.css";
+import { nanoid } from "nanoid";
 
 export default function Facts(props) {
-    const facts = [
-        props.facts.one,
-        props.facts.two,
-        props.facts.three,
-        props.facts.four,
-    ];
+    // gather the given planet's facts in an array
+    let facts = [];
+    // fact comes from an object
+    for (const fact in props.facts) {
+        facts.push(props.facts[fact]);
+    }
 
-    const arrayOfFacts = facts.map((fact, index) => (
-        <div key={index} className="fact common">
-            <span className="property">{fact.property.toUpperCase()}</span>
-            <span className="value">{fact.value.toUpperCase()}</span>
+    return (
+        <div className="facts-container">
+            {/* each div is a fact of the planet */}
+            {facts.map((fact) => (
+                <div key={nanoid()} className="fact-container">
+                    <span>{fact.property.toUpperCase()}</span>
+                    <span>{fact.value.toUpperCase()}</span>
+                </div>
+            ))}
         </div>
-    ));
-
-    return <div className="facts common">{arrayOfFacts}</div>;
+    );
 }
