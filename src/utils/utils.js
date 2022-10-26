8d0ddc47dfed9fb0903bd19b5999bbd4c7bff9ab
massuhcodes <massuhcodes@gmail.com>
2022-10-25 23:51:45 -0400

    Modified utils file

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,30 @@
+// element that opens up `overhead`
+let hamburgerEl;
+// element that is opened or compressed
+let overheadEl;
+
+// wait for everything to load to assign
+window.onload = (event) => {
+    hamburgerEl = document.getElementById("hamburger");
+    overheadEl = document.getElementById("overhead");
+};
+
 /**
  * Expands Header component's overhead container when hamburger is clicked-on
  */
 export function expandOverhead() {
-    document.getElementById("hamburger").style.opacity = "0.1";
-    document.getElementById("overhead").classList.add("expanded");
+    hamburgerEl.style.opacity = "0.1";
+    hamburgerEl.style.cursor = "auto";
+    overheadEl.classList.add("expanded");
 }
-
+/**
+ * Compress Header component's overhead container when a planet has been picked
+ */
+export function compressOverhead() {
+    hamburgerEl.style.opacity = "1";
+    hamburgerEl.style.cursor = "pointer";
+    overheadEl.classList.remove("expanded");
+}
 /**
  * Features that will be used in Header component (mobile layout) and Information component (tablet, desktop, etc. layouts)
  */
