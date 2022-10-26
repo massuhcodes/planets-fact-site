c18aaa9593122ad285bc8e0286de476ce9f5b003
massuhcodes <massuhcodes@gmail.com>
2022-10-26 11:32:19 -0400

    Modified Header jsx file by adding appropriate class names

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,35 +6,33 @@ import chevron from "/src/assets/icon-chevron.svg";
 
 export default function Header(props) {
     // planet choices for mobile layout
-    const verticalPlanetChoices = props.planets.map((planet, index) => {
-        return (
-            <div
-                key={nanoid()}
-                className="vertical-planet-choice"
-                onClick={() => props.getChoice(index)}
-            >
-                <div className="dot-name-container">
-                    <span
-                        className="dot"
-                        style={{ backgroundColor: planet.color }}
-                    />
-                    <span className="name">{planet.name.toUpperCase()}</span>
-                </div>
-                <img src={chevron} />
+    const verticalPlanetChoices = props.planets.map((planet, index) => (
+        <li
+            key={nanoid()}
+            className="vertical-planet-choice-container"
+            onClick={() => props.getPlanet("vertical", index)}
+        >
+            <div className="dot-name-container">
+                <span
+                    className="dot"
+                    style={{ backgroundColor: planet.color }}
+                />
+                <span className="name">{planet.name.toUpperCase()}</span>
             </div>
-        );
-    });
+            <img src={chevron} />
+        </li>
+    ));
 
     // planet choices for tablet, desktop, etc. layouts
     const horizontalPlanetChoices = props.planets.map((planet, index) => (
         <li
             key={nanoid()}
             className="horizontal-planet-choice"
-            onClick={() => props.getChoice(index)}
+            onClick={() => props.getPlanet("horizontal", index)}
             style={
-                planet === props.chosenPlanet
+                planet.name === props.chosenPlanet.name
                     ? { color: props.color }
-                    : { color: "#ffffff80" }
+                    : {}
             }
         >
             {planet.name.toUpperCase()}
@@ -42,14 +40,21 @@ export default function Header(props) {
     ));
 
     // Header component's features (mobile)
-    const featureElements = features.map((feature) => (
+    const headerFeatures = features.map((feature) => (
         <li
             key={nanoid()}
             id={feature}
+            className={`header-feature-wrapper ${
+                props.chosenFeature === feature ? "selected" : ""
+            }`}
             onClick={() => props.switchFeatureTo(feature)}
             style={
-                props.feature === feature
-                    ? { color: "#fff", borderBottomColor: props.color }
+                props.chosenFeature === feature
+                    ? {
+                          color: "white",
+                          "--selected-feature-after-background-color":
+                              props.color,
+                      }
                     : {}
             }
         >
@@ -58,19 +63,22 @@ export default function Header(props) {
     ));
 
     return (
-        <div className="Header-container">
+        <div className="header-container">
             <header>
                 <h2>{"the planets".toUpperCase()}</h2>
+                {/* will be removed for larger-than-mobile screens */}
                 <img id="hamburger" src={hamburger} onClick={expandOverhead} />
-                {/** tablet, desktop, etc. */}
+                {/* tablet, desktop, etc. */}
                 <ul className="horizontal-planet-choice-container">
                     {horizontalPlanetChoices}
                 </ul>
             </header>
-            <div className="overhead-container" id="overhead">
+            {/* will be removed for larger-than-mobile screens */}
+            <ul className="overhead-container" id="overhead">
                 {verticalPlanetChoices}
-            </div>
-            <ul className="features">{featureElements}</ul>
+            </ul>
+            {/* will be removed for larger-than-mobile screens */}
+            <ul className="header-features-container">{headerFeatures}</ul>
         </div>
     );
 }
