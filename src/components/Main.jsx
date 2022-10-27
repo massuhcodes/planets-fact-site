4de3153b971f3fa0bb3d35dbb208b5fb769aa36e
massuhcodes <massuhcodes@gmail.com>
2022-10-26 23:06:59 -0400

    Modified Main jsx file by adding comments

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,33 @@
-import { useContext } from "react";
+// Main.jsx
+
 import "/src/styles/Main.css";
+import { useContext } from "react";
 import Planet from "/src/components/Planet";
-import Summary from "/src/components/Summary";
+import Information from "/src/components/Information";
 import Facts from "/src/components/Facts";
 import { PlanetContext } from "./App";
 
 export default function Main() {
-    const planet = useContext(PlanetContext);
+    // useContext is useful because it bypasses the need to propagate props through the hierchy
+    const [planet, feature, switchFeatureTo] = useContext(PlanetContext);
     return (
         <main>
-            <Planet meta={planet.meta} />
-            <Summary summary={planet.summary} name={planet.name} />
+            {/* provide the data each component requires through props */}
+            <div className="planet-information-container">
+                <Planet
+                    images={planet.featureImages}
+                    feature={feature}
+                    name={planet.name}
+                />
+                <Information
+                    info={planet.info[feature]}
+                    name={planet.name}
+                    linkColor={planet.color}
+                    feature={feature}
+                    featureBackgroundColor={planet.backgroundColor}
+                    switchFeatureTo={switchFeatureTo}
+                />
+            </div>
             <Facts facts={planet.facts} />
         </main>
     );
