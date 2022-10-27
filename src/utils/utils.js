b25ad87aff7196ff707b111652b6cc2c8f23120c
massuhcodes <massuhcodes@gmail.com>
2022-10-27 16:11:51 -0400

    Added handlePlanetFade function to handle fade animation for planet or feature change

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -20,6 +20,27 @@ export function compressOverhead() {
     hamburgerEl.style.cursor = "pointer";
     overheadEl.classList.remove("expanded");
 }
+
+/**
+ *
+ * @param {object} update - a function given by App.jsx that is for updating state (could be for updating planet or feature)
+ * @param {String} state - a String given by App.jsx that is for updating state (could be either planet or feature)
+ */
+export function handlePlanetFade(update, state) {
+    const planetContainerEl = document.getElementById("planet-container");
+    // perform the animation
+    planetContainerEl.classList.add("fadeOut");
+    planetContainerEl.classList.remove("fadeIn");
+    // wait until animation ends (which is for .4s) then begin
+    setTimeout(() => update(state), 400);
+    // wait until animation ends (which is again .4s) then begin
+    setTimeout(() => {
+        planetContainerEl.classList.add("fadeIn");
+        planetContainerEl.classList.remove("fadeOut");
+    }, 400);
+    // the total animation time is 1.6s
+}
+
 /**
  * Features that will be used in Header component (mobile layout) and Information component (tablet, desktop, etc. layouts)
  */
