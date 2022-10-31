2f7215c924a7634ce0a110e8bad459c398fcb246
massuhcodes <massuhcodes@gmail.com>
2022-10-30 20:09:37 -0400

    Added title and alt to image

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -3,19 +3,35 @@
 import "/src/styles/Planet.css";
 
 export default function Planet(props) {
+    const name = props.name[0].toUpperCase() + props.name.slice(1);
     return (
         <div id="planet-container" className="planet-container initialFadeIn">
             <img
+                title={
+                    props.feature === "overview"
+                        ? `The planet ${name}.`
+                        : props.feature === "structure"
+                        ? `${name}'s interior structure.`
+                        : `${name}'s surface.`
+                }
+                alt={
+                    props.feature === "overview"
+                        ? `The planet ${name}.`
+                        : props.feature === "structure"
+                        ? `${name}'s interior structure.`
+                        : "${name}'s surface."
+                }
                 className={props.name}
                 src={
-                    props.feature === "surface"
-                        ? props.images.overview
-                        : props.images[props.feature]
+                    props.feature === "overview" || props.feature === "surface"
+                        ? `/src/assets/planet-${name}.svg`
+                        : `/src/assets/planet-${name}-internal.svg`
                 }
+                alt={`image of ${name}`}
             />
             <img
                 className="with-magnifier"
-                src={props.images.surface}
+                src={`/src/assets/geology-${name}.png`}
                 style={
                     props.feature === "surface"
                         ? { opacity: "1" }
