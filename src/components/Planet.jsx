ecc312f5dd26f74a964b4a2e6fe1a3ded44b3c28
massuhcodes <massuhcodes@gmail.com>
2022-10-31 15:47:36 -0400

    Renamed planet component file path and added wrapper to parent div

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,36 +1,41 @@
 // Planet.jsx
 
 import "/src/styles/Planet.css";
-import { Mercury } from "./planets/Mercury";
-import { MercuryInternal } from "./planets/MercuryInternal";
+import { Mercury } from "./svgs/planets/Mercury";
+import { MercuryInternal } from "./svgs/planets/MercuryInternal";
 import mercuryMagnifier from "../assets/geology-mercury.png";
-import { Venus } from "./planets/Venus";
-import { VenusInternal } from "./planets/VenusInternal";
+import { Venus } from "./svgs/planets/Venus";
+import { VenusInternal } from "./svgs/planets/VenusInternal";
 import venusMagnifier from "../assets/geology-venus.png";
-import { Earth } from "./planets/Earth";
-import { EarthInternal } from "./planets/EarthInternal";
+import { Earth } from "./svgs/planets/Earth";
+import { EarthInternal } from "./svgs/planets/EarthInternal";
 import earthMagnifier from "../assets/geology-earth.png";
-import { Mars } from "./planets/Mars";
-import { MarsInternal } from "./planets/MarsInternal";
+import { Mars } from "./svgs/planets/Mars";
+import { MarsInternal } from "./svgs/planets/MarsInternal";
 import marsMagnifier from "../assets/geology-mars.png";
-import { Jupiter } from "./planets/Jupiter";
-import { JupiterInternal } from "./planets/JupiterInternal";
+import { Jupiter } from "./svgs/planets/Jupiter";
+import { JupiterInternal } from "./svgs/planets/JupiterInternal";
 import jupiterMagnifier from "../assets/geology-jupiter.png";
-import { Saturn } from "./planets/Saturn";
-import { SaturnInternal } from "./planets/SaturnInternal";
+import { Saturn } from "./svgs/planets/Saturn";
+import { SaturnInternal } from "./svgs/planets/SaturnInternal";
 import saturnMagnifier from "../assets/geology-saturn.png";
-import { Uranus } from "./planets/Uranus";
-import { UranusInternal } from "./planets/UranusInternal";
+import { Uranus } from "./svgs/planets/Uranus";
+import { UranusInternal } from "./svgs/planets/UranusInternal";
 import uranusMagnifier from "../assets/geology-uranus.png";
-import { Neptune } from "./planets/Neptune";
-import { NeptuneInternal } from "./planets/NeptuneInternal";
+import { Neptune } from "./svgs/planets/Neptune";
+import { NeptuneInternal } from "./svgs/planets/NeptuneInternal";
 import neptuneMagnifier from "../assets/geology-neptune.png";
 
 export default function Planet(props) {
+    // shorten
     const name = props.name;
-    const capitalizedName = props.name[0].toUpperCase() + props.name.slice(1);
+    // for title and alt
+    const capitalizedName = name[0].toUpperCase() + name.slice(1);
+    // planet component to show based on user selection
     let planetComponent;
+    // planet's magnifier when user wants to view surface feature
     let magnifier;
+    // get both planet component and magnifier
     switch (name) {
         case "mercury":
             planetComponent =
@@ -88,7 +93,7 @@ export default function Planet(props) {
     return (
         <div id="planet-container" className="planet-container initialFadeIn">
             <div
-                className={props.name}
+                className={`${name}-wrapper`}
                 title={
                     props.feature === "overview"
                         ? `The planet ${name}.`
