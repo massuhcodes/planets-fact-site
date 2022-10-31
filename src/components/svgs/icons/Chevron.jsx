d3b5b3799e635c603a0fee4edb2d606b8e1b2aef
massuhcodes <massuhcodes@gmail.com>
2022-10-31 15:28:50 -0400

    Added a folder to hold React Components svgs for both planets and icons

---
+++ src/components/svgs/icons/Chevron.jsx
@@ -0,0 +1,9 @@
+// Chevron.jsx
+
+export function Chevron() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6 8">
+            <path fill="none" stroke="#FFF" opacity=".4" d="M1 0l4 4-4 4" />
+        </svg>
+    );
+}
